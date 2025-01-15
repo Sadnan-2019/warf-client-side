8b1d4f97e472f70f008ed75c0acb29b9d63f3f93
Sadnan-2019 <sadnan.wow@gmail.com>
2025-01-15 16:40:50 +0600

    start

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3346d3",
+          secondary: "#0287D0",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
+ 
